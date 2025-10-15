65f7e6d10bec27e813d40217c990a6515ad39c05
FabioCoutinho <fabiofarias549@gmail.com>
2025-10-15 15:43:20 -0300

    create search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
 
+import classes from "./App.module.css";
+
 function App() {
   return (
-    <div className="APP">
+    <div className={classes.app}>
       <h1>GitHub Finder</h1>
       <Outlet />
     </div>
---
+++ src/components/Search.tsx
@@ -0,0 +1,30 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+import classes from "./Search.module.css";
+
+const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+  return (
+    <div className={classes.container__search}>
+      <h2>Busque por um usuário:</h2>
+      <p>Conheça seus melhores repositórios</p>
+      <div className={classes.container_input__search}>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch fill="#000"/>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
